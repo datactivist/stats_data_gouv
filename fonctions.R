0eaa0004ca7eba9e4a997e4df28f6f0b0f7c24b3
Joel Gombin <joel.gombin@gmail.com>
2017-03-30 02:10:51 +0200

    mots-clés referrers

--- fonctions.R
+++ fonctions.R
@@ -165,3 +165,26 @@ getSiteSearchKeywords <- function(idSite = 1,
       mutate(exit_rate = stringr::str_replace(exit_rate, "%", "") %>% as.double()) %>%
       mutate(date = lubridate::ymd(date))
   
+  ## mots-clés de recherche (referrers)
+  
+getReferrersSearchKeywords <- function(idSite = 1,
+                                    period = "day",
+                                    date = "last30",
+                                    url = "http://stats.data.gouv.fr/index.php?module=API") {
+    request <- GET(url = url, 
+                   query = list(
+                     method = "Referrers.getKeywords",
+                     idSite = idSite,
+                     period = period,
+                     date = date,
+                     expanded = 1,
+                     flat = 1,
+                     filter_limit = -1,
+                     format = "json"))
+    content(request) %>% 
+      map_df(~ map_df(., function(x) {
+        data_frame(label = x$label,
+                   nb_visits = x$nb_visits)
+      }), .id = "date") %>% 
+      mutate(date = lubridate::ymd(date))
+}
